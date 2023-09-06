3891935a3ee8fc1b198d866aaddcf2d3711a1844
Sai Barath <saibarathdeep@outlook.com>
2023-09-06 07:45:44 +0530

    aded header sub components like seach and other details

---
+++ src/Pages/Header.jsx
@@ -0,0 +1,24 @@
+import { Box, Text } from "@chakra-ui/react";
+import SearchBar from "./commonComponents/SearchBar";
+import CustomImageViewer from "./commonComponents/ImageLoade";
+import HeaderImage from '../icons/headerIcon.png'
+import { ThemeSwitcher } from "./commonComponents/ModeSwitcherButton";
+
+const Header = () => {
+    return (
+        <Box className="flex flex-col md:flex-row gap-4 items-center px-2 justify-between min-h-[100px]  h-[10%] w-full" >
+            <Box className="flex gap-2 items-center">
+                <CustomImageViewer imgSrc={HeaderImage} />
+                <Text>
+                    MangaHarbor
+                </Text>
+                <ThemeSwitcher />
+
+            </Box>
+            <SearchBar />
+
+        </Box >
+    );
+}
+
+export default Header;
