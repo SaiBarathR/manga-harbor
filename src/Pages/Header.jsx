eeb199cdcfe6cdc13d3e65e7502c932fc4a9fd5d
Sai Barath <saibarathdeep@outlook.com>
2023-09-09 06:29:40 +0530

    added max limit of width to 2560px

--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -6,17 +6,15 @@ import { ThemeSwitcher } from "./commonComponents/ModeSwitcherButton";
 
 const Header = () => {
     return (
-        <Box className="flex flex-col md:flex-row gap-4 items-center px-2 justify-center md:justify-between min-h-[100px]  h-[10%] w-full" >
+        <Box className="flex flex-col md:flex-row gap-4 items-center px-2 justify-center md:justify-between min-h-[100px]  h-[10%] max-h-[100px] w-full" >
             <Box className="flex gap-2 items-center">
                 <CustomImageViewer imgSrc={HeaderImage} />
                 <Text>
                     MangaHarbor
                 </Text>
                 <ThemeSwitcher />
-
             </Box>
             <SearchBar />
-
         </Box >
     );
 }
