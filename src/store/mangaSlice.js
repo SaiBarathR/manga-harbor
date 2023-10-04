ccd53de9cc0ff686e158337ef4b4909d3039dde4
Sai Barath <saibarathdeep@outlook.com>
2023-10-04 22:15:34 +0530

    added fetchMangaById thunk in manga slice

--- src/store/mangaSlice.js
+++ src/store/mangaSlice.js
@@ -1,17 +1,45 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import MangaService from '../service/mangaService'
 
 const initialState = {
-    manga: {}
+    mangaDetails: {},
+    loading: false,
 }
+
+export const fetchMangaById = createAsyncThunk(
+    'search/fetchMangaById',
+    async (id) => {
+        try {
+            const resp = await MangaService.get('manga', id)
+            return resp
+        }
+        catch (e) {
+            console.log(e)
+        }
+    }
+)
 
 export const mangaSlice = createSlice({
     name: 'manga',
     initialState,
     reducers: {
         setManga: (state, action) => {
-            state.manga = action.payload;
+            state.mangaDetails = action.payload;
         }
     },
+    extraReducers: (builder) => {
+        builder.addCase(fetchMangaById.fulfilled, (state, action) => {
+            state.mangaDetails = { ...state.mangaDetails, ...action.payload.mangaData.data }
+            state.loading = false;
+        })
+        builder.addCase(fetchMangaById.pending, (state) => {
+            state.loading = true;
+        })
+        builder.addCase(fetchMangaById.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.error.message;
+        })
+    }
 })
 
 export const { setManga } = mangaSlice.actions
