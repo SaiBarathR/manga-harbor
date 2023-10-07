fcd4360bb7d85302fb298ccb0efc1f2bffda6655
Sai Barath <saibarathr@gmail.com>
2023-10-07 18:22:33 +0530

    added a new slice to handle manga download

---
+++ src/store/mangaDownloader.js
@@ -0,0 +1,85 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import MangaService from '../service/mangaService'
+
+const initialState = {
+    volumesToDownload: [],
+    loading: false,
+}
+
+const downloadManga = async (blob) => {
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'manga.zip';
+    document.body.appendChild(a);
+    a.click();
+    window.URL.revokeObjectURL(url);
+}
+
+
+export const downloadMangaById = createAsyncThunk(
+    'manga/downloadMangaById',
+    async (id) => {
+        try {
+            const blob = await MangaService.download('download', id)
+            downloadManga(blob)
+        } catch (error) {
+            console.error('An error occurred:', error);
+        }
+    }
+)
+
+export const dowwnloadMangaByVolume = createAsyncThunk(
+    'manga/dowwnloadMangaByVolume',
+    async (data, { dispatch, getState }) => {
+        try {
+            const blob = await MangaService.download('download', data.id + '/volume/' + data.volumeNumber)
+            downloadManga(blob)
+        } catch (error) {
+            console.error('An error occurred:', error);
+        }
+    }
+)
+
+
+export const mangaDownloaderSlice = createSlice({
+    name: 'mangaDownloader',
+    initialState,
+    reducers: {
+        addVolumeToDownloadQueue: (state, action) => {
+            if (action.payload.length) {
+                state.volumesToDownload = [...state.volumesToDownload, ...action.payload]
+            }
+            else {
+                state.volumesToDownload.push(action.payload);
+            }
+        }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(downloadMangaById.fulfilled, (state, action) => {
+            state.loading = false;
+        })
+        builder.addCase(downloadMangaById.pending, (state) => {
+            state.loading = true;
+        })
+        builder.addCase(downloadMangaById.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.error.message;
+        })
+        builder.addCase(dowwnloadMangaByVolume.fulfilled, (state, action) => {
+            state.volumesToDownload.shift()
+            state.loading = false;
+        })
+        builder.addCase(dowwnloadMangaByVolume.pending, (state) => {
+            state.loading = true;
+        })
+        builder.addCase(dowwnloadMangaByVolume.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.error.message;
+        })
+    }
+})
+
+export const { addVolumeToDownloadQueue } = mangaDownloaderSlice.actions
+
+export default mangaDownloaderSlice.reducer
