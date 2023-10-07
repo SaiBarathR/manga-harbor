857c0331443735e0cab1ffb4e68a065fe2703b4a
Sai Barath <saibarathr@gmail.com>
2023-10-07 18:23:33 +0530

    store values will now persist through reload

--- src/store/store.js
+++ src/store/store.js
@@ -15,6 +15,36 @@ export const rootReducer = (state, action) => {
     return combinedMangaAppReducers(state, action);
 };
 
+const loadState = () => {
+    try {
+        const currentReduxStates = localStorage.getItem('state');
+        if (currentReduxStates === null) {
+            return undefined;
+        }
+        return JSON.parse(currentReduxStates);
+    } catch (e) {
+        console.log("error loadin redux state from local storage", e)
+    }
+};
+
+const saveState = (state) => {
+    try {
+        const currentReduxStates = JSON.stringify(state);
+        localStorage.setItem('state', currentReduxStates);
+    } catch (e) {
+        console.log("Error saving redux state before reload", e)
+    }
+};
+
+window.addEventListener('beforeunload', () => {
+    saveState(store.getState());
+});
+
+window.removeEventListener('beforeunload', () => {
+    saveState(store.getState());
+});
+
 export const store = configureStore({
     reducer: rootReducer,
+    preloadedState: loadState()
 })
