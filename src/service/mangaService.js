352b0b260b75b7f5bca9e4114231790de8231af0
Sai Barath <saibarathdeep@outlook.com>
2023-10-10 10:47:48 +0530

    using axios for manga service

--- src/service/mangaService.js
+++ src/service/mangaService.js
@@ -1,27 +1,35 @@
 import app from '../config/app.json'
+import axios from 'axios';
 
 const MangaService = (function () {
 
-    const baseURL = app.baseUrl['springBoot']
-    var service = {};
+    const API = axios.create({
+        baseURL: app.baseUrl['springBoot']
+    });
+    let service = {};
     const urls = app.urls
 
     service.get = async function (key, params = '', type = 'json', headers = '') {
-        const resp = await fetch(baseURL + urls[key] + params)
-        if (!resp.ok) throw new Error('Network resp was not ok');
-        return type === 'image' ? resp.arrayBuffer() : resp.json() || resp;
+        const resp = await API.get(urls[key] + params, { responseType: type === 'json' ? 'json' : 'arraybuffer' })
+        if (resp.status === 200) {
+            return resp.data || resp;
+        }
     }
 
     service.download = async function (key, params = '') {
-        const resp = await fetch(baseURL + urls[key] + params, { method: 'GET', responseType: 'blob' });
-        if (!resp.ok) throw new Error('Network resp was not ok');
-        if (resp.status === 200) {
-            for (let header of resp.headers.entries()) {
-                if (header[0] === 'content-disposition') {
-                    const fileName = header[1].split('filename=')[1];
-                    return { fileName: fileName, file: await resp.blob() }
-                }
+        const resp = await API.get(urls[key] + params, {
+            responseType: 'blob',
+            headers: {
+                'Content-Type': 'application/zip',
+                'Accept': 'application/zip'
+            },
+            onDownloadProgress: (progressEvent) => {
+                // let percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total); // you can use this to show user percentage of file downloaded
             }
+        });
+        if (resp.status === 200) {
+            const fileName = resp.headers['content-disposition'].split('filename=')[1];
+            return { fileName: fileName, file: await resp.data }
         }
     }
 
