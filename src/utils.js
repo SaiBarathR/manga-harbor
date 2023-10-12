5a09427142dc983d9443d5e2dd93949a51bde427
Sai Barath <saibarathr@gmail.com>
2023-10-13 01:34:37 +0530

    added utils file for common function

---
+++ src/utils.js
@@ -0,0 +1,17 @@
+import { UNITS } from "./config/constants";
+
+export const downloadMangaUsingFile = async (file, fileName) => {
+    const url = window.URL.createObjectURL(file);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+    window.URL.revokeObjectURL(url);
+}
+
+export function formatBytes(bytes) {
+    let l = 0, n = parseInt(bytes, 10) || 0;
+    while (n >= 1000 && ++l) n = n / 1000;
+    return (n.toFixed(n < 10 && l > 0 ? 1 : 0) + ' ' + UNITS[l]);
+}
