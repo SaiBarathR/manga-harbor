ba3c4743cce5491af66825334f9b010ee9482a12
Sai Barath <saibarathr@gmail.com>
2023-10-13 01:34:52 +0530

    added data units in constants

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,9 +1,10 @@
-export const mangaStatusColors = {
+export const MangaStatusColors = {
     'ongoing': "purple",
     'completed': "green",
     'hiatus': "yellow",
     'cancelled': "red"
 }
 
+export const UNITS = ['bytes', 'KB', 'MB', 'GB'];
 
 
