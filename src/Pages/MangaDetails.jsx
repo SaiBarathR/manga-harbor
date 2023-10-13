bdae149c438d0a63ee132ec5f4f27fbd5ddfa965
Sai Barath <saibarathr@gmail.com>
2023-10-13 22:00:44 +0530

    added NewItemToDownloadQueue option in volume details

--- src/Pages/MangaDetails.jsx
+++ src/Pages/MangaDetails.jsx
@@ -1,4 +1,4 @@
-import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Button, ButtonGroup, Grid, GridItem, Heading, IconButton, Image, List, ListItem, Skeleton, Text, Tooltip } from '@chakra-ui/react'
+import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Button, ButtonGroup, CircularProgress, Grid, GridItem, Heading, IconButton, Image, List, ListItem, Skeleton, Text, Tooltip } from '@chakra-ui/react'
 import React, { useEffect, } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchMangaById, fetchVolumeList } from '../store/mangaSlice';
@@ -6,7 +6,7 @@ import { TagRenderer } from './commonComponents/SearchBar';
 import useMangaImage from '../hooks/useMangaImage';
 import { MangaStatusColors } from '../config/constants';
 import { BellIcon, DownloadIcon, StarIcon } from '@chakra-ui/icons';
-import { downloadMangaByVolumeOrChapter } from '../store/mangaDownloaderSlice';
+import { addNewItemToDownloadQueue, downloadMangaByVolumeOrChapter } from '../store/mangaDownloaderSlice';
 
 export default function MangaDetails() {
     const mangaDetails = useSelector((state) => state.manga.mangaDetails);
@@ -26,7 +26,7 @@ export default function MangaDetails() {
     </Skeleton> : <Box className='w-[96%]'>
         <Box className='flex flex-col items-center'>
             <MangaDetailsHeader manga={mangaDetails} dispatch={dispatch} />
-            <ToolbarItems id={mangaDetails.id} />
+            <ToolbarItems id={mangaDetails.id} title={mangaDetails.title} />
         </Box>
     </Box>)
 }
@@ -43,8 +43,8 @@ const MangaDetailsHeader = ({ manga, dispatch }) => {
     return <Box _dark={{ bg: 'blackAlpha.600' }} bg={'#adcdf7'}
         className="flex w-full p-1 items-center  my-2 mx-8 lg:mx-10 md:p-2 lg:p-4 rounded-md shadow-xl">
         {!imageData ? <Box width={'252px'}> <Skeleton height={'330px'} width={'202px'} /> </Box> :
-            <Image rounded={'lg'} maxW={'10%'} maxH={'10%'} src={imageData} alt='m' display={!imageData && 'none'}
-            />}
+            <Image rounded={'lg'} maxW={'10%'} maxH={'10%'} src={imageData} alt='m' display={!imageData && 'none'} />
+        }
         <Box className="ml-8 self-start flex flex-col">
             <Tooltip label={manga.title} hasArrow arrowSize={10} placement="top" >
                 <Heading >{manga.title}</Heading>
@@ -65,13 +65,14 @@ const MangaDetailsHeader = ({ manga, dispatch }) => {
     </Box>
 }
 
-const ToolbarItems = ({ id }) => {
+const ToolbarItems = ({ id, title }) => {
     const volumes = useSelector((state) => state.manga.volumes);
     const dispatch = useDispatch()
 
     const onClickDownload = async (vol, chapter = null) => {
         let volume = (vol === 'none') ? 0 : vol;
         const params = !chapter ? id + '/' + volume : id + '/' + volume + '/' + chapter;
+        dispatch(addNewItemToDownloadQueue({ name: title, volume: volume, chapter: chapter }))
         dispatch(downloadMangaByVolumeOrChapter(params))
     };
 
@@ -89,8 +90,10 @@ const ToolbarItems = ({ id }) => {
                         <List spacing={3}  >
                             <ListItem>
                                 <ButtonGroup rounded={'lg'} isAttached variant='outline' onClick={() => onClickDownload(volume.volume)}>
-                                    <Button> Volume {volume.volume}</Button>
-                                    <IconButton icon={<DownloadIcon />} />
+                                    <Button> Preparing Volume {volume.volume}</Button>
+                                    <IconButton  >
+                                        <DownloadIcon />
+                                    </IconButton>
                                 </ButtonGroup>
                             </ListItem>
                             {volume.chapters.length > 0 && volume.chapters.map((chapter) => <ListItem key={chapter.chapter} className="flex gap-3 items-center">
