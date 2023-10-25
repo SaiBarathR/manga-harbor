3889f0c13450a5c72a087968c98e069053c03d88
Sai Barath <saibarathdeep@outlook.com>
2023-10-25 11:13:46 +0530

    decoding filename when downloading

--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,7 @@ export const downloadMangaUsingFile = async (file, fileName) => {
     const url = window.URL.createObjectURL(file);
     const a = document.createElement('a');
     a.href = url;
-    a.download = fileName;
+    a.download = decodeURIComponent(fileName).replace(/\+/g, ' ') || 'manga.zip';
     document.body.appendChild(a);
     a.click();
     window.URL.revokeObjectURL(url);
