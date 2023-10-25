a6e7fc78302f71c003a8b6b718f95f3ed0e1c583
Sai Barath <saibarathdeep@outlook.com>
2023-10-25 11:13:12 +0530

    commented store values saving in local storage logic for refresh

--- src/store/store.js
+++ src/store/store.js
@@ -16,36 +16,36 @@ export const rootReducer = (state, action) => {
     return combinedMangaAppReducers(state, action);
 };
 
-const loadState = () => {
-    try {
-        const currentReduxStates = localStorage.getItem('state');
-        if (currentReduxStates === null) {
-            return undefined;
-        }
-        return JSON.parse(currentReduxStates);
-    } catch (e) {
-        console.log("error loading redux state from local storage", e)
-    }
-};
+// const loadState = () => {
+//     try {
+//         const currentReduxStates = localStorage.getItem('state');
+//         if (currentReduxStates === null) {
+//             return undefined;
+//         }
+//         return JSON.parse(currentReduxStates);
+//     } catch (e) {
+//         console.log("error loading redux state from local storage", e)
+//     }
+// };
+//
+// const saveState = (state) => {
+//     try {
+//         const currentReduxStates = JSON.stringify(state);
+//         localStorage.setItem('state', currentReduxStates);
+//     } catch (e) {
+//         console.log("Error saving redux state before reload", e)
+//     }
+// };
 
-const saveState = (state) => {
-    try {
-        const currentReduxStates = JSON.stringify(state);
-        localStorage.setItem('state', currentReduxStates);
-    } catch (e) {
-        console.log("Error saving redux state before reload", e)
-    }
-};
-
-window.addEventListener('beforeunload', () => {
-    saveState(store.getState());
-});
-
-window.removeEventListener('beforeunload', () => {
-    saveState(store.getState());
-});
+// window.addEventListener('beforeunload', () => {
+//     saveState(store.getState());
+// });
+//
+// window.removeEventListener('beforeunload', () => {
+//     saveState(store.getState());
+// });
 
 export const store = configureStore({
     reducer: rootReducer,
-    preloadedState: loadState()
+    // preloadedState: loadState()
 })
