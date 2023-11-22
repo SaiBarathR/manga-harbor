10d02ce76b2163e2cf7b1747214c1f0e739a2e39
Sai Barath <saibarathr@gmail.com>
2023-11-22 16:42:44 +0530

    switched from cra to vite

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import svgr from "vite-plugin-svgr";
+import reactRefresh from 'vite-plugin-react-refresh';
+
+export default defineConfig({
+    plugins: [react(), reactRefresh(), svgr()],
+    server: {
+        host: true,
+        strictPort: true,
+        port: 3034
+    },
+    define: {
+        global: 'window',
+    },
+    build: {
+        outDir: "mh"
+    },
+    base: './'
+})
