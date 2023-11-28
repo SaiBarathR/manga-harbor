6f44c6c78ae0602882e6c575449733bb4c93074e
Sai Barath <saibarathdeep@outlook.com>
2023-11-28 18:25:21 +0530

    Remove unused import statement

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Center, Container, useColorModeValue } from '@chakra-ui/react';
 import './App.css';
-import Home from './Pages/Home';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/route';
 import { Box } from '@chakra-ui/react';
