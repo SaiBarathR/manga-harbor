b80f81565dea5ded8fc451370afa50cc99b22971
Sai Barath <saibarathdeep@outlook.com>
2023-11-28 17:42:47 +0530

    Add lazy loading for DownloadManager component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,16 @@
+import { Suspense, lazy } from "react";
 import Header from "./Header"
-import DownloadManager from "./DownloadManager";
+// import DownloadManager from "./DownloadManager";
+const DownloadManager = lazy(() => import("./DownloadManager"));
 import { Outlet } from "react-router-dom";
+import Loader from "../common/loader";
 
 const Home = () => {
-    return <>
-        <Header />
+    return <Suspense fallback={<Loader />}>
+        < Header />
         <Outlet />
         <DownloadManager />
-    </>
+    </Suspense >
 }
 
 export default Home;
