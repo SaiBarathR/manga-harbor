4e0d11456a0a1a0070e831def540f89c06a1be20
Sai Barath <saibarathr@gmail.com>
2023-11-28 18:03:17 +0530

    Add eslint plugin to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
 import reactRefresh from 'vite-plugin-react-refresh';
+import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
-    plugins: [react(), reactRefresh(), svgr()],
+    plugins: [react(), reactRefresh(), svgr(), eslint()],
     server: {
         host: true,
         strictPort: true,
