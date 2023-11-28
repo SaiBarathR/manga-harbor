2f10ea25a2ba2415f6f26b05f89ccd02c4bc50dd
Sai Barath <saibarathdeep@outlook.com>
2023-11-28 19:05:51 +0530

    Fix import statement in route.jsx

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import React, { Suspense, lazy } from 'react';
+import { Suspense, lazy } from 'react';
 import Home from "../Pages/Home";
 import Loader from "../common/loader";
 
