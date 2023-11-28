086fd10125ac057d2c117f0e518415450375bf3a
Sai Barath <saibarathr@gmail.com>
2023-11-28 18:03:28 +0530

    Import DownloadManager and fix formatting in Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,9 @@
+import { Outlet } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import Header from "./Header"
-// import DownloadManager from "./DownloadManager";
-const DownloadManager = lazy(() => import("./DownloadManager"));
-import { Outlet } from "react-router-dom";
 import Loader from "../common/loader";
+
+const DownloadManager = lazy(() => import("./DownloadManager"));
 
 const Home = () => {
     return <Suspense fallback={<Loader />}>
