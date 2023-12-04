ce167ba4d5f3034eca0cede724a695d7087968ff
Sai Barath <saibarathr@gmail.com>
2023-12-04 18:06:40 +0530

    Add functions to get manga name and color name, and construct manga details

--- src/utils.js
+++ src/utils.js
@@ -15,3 +15,59 @@ export function formatBytes(bytes) {
     while (n >= 1000 && ++l) n = n / 1000;
     return (n.toFixed(n < 10 && l > 0 ? 1 : 0) + ' ' + UNITS[l]);
 }
+
+function getMangaName(attributes) {
+    if (attributes) {
+        const title = attributes.title;
+        if (title && title.en) {
+            return title.en;
+        } else {
+            const altTitles = attributes.altTitles;
+            if (altTitles) {
+                for (const altTitle of altTitles) {
+                    if (altTitle.en) {
+                        return altTitle.en;
+                    }
+                }
+            }
+        }
+    }
+}
+
+function getColorName(rating) {
+    switch (true) {
+        case (rating < 1):
+            return "red";
+        case (rating < 2):
+            return "orange";
+        case (rating < 3):
+            return "yellow";
+        case (rating < 4):
+            return "pink";
+        case (rating < 5):
+            return "gray";
+        case (rating < 6):
+            return "gray";
+        case (rating < 7):
+            return "linkedin";
+        case (rating < 8):
+            return "purple";
+        case (rating < 9):
+            return "green";
+        case (rating < 10):
+            return "yellow";
+        default:
+            return "Invalid Rating";
+    }
+}
+export function constructMangaDetails(manga, stats) {
+    return {
+        title: getMangaName(manga.attributes),
+        image: manga.image,
+        year: manga.attributes.year,
+        status: manga.attributes.status,
+        id: manga.id,
+        rating: stats ? { value: stats[manga.id].rating.average, color: getColorName(stats[manga.id].rating.average) } : {},
+        follows: stats ? stats[manga.id].follows : null,
+    }
+}
