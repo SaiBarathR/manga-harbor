57aebf388e87b55fbc4252403c27d31398019b05
Sai Barath <saibarathr@gmail.com>
2023-12-07 18:15:35 +0530

    Refactor Header component styles

--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -4,24 +4,17 @@ import CustomImageViewer from "./commonComponents/ImageLoade";
 import HeaderImage from '../icons/headerIcon.png'
 import { ThemeSwitcher } from "./commonComponents/ModeSwitcherButton";
 import { useLocation, useNavigate } from "react-router-dom";
-import { useEffect } from "react";
 
 const Header = () => {
 
     const { pathname } = useLocation()
     const navigate = useNavigate()
-
     const isMangaDetailsPage = pathname.includes('manga')
     const onClickHeader = () => navigate('/');
 
-    useEffect(() => {
-        console.log('home created')
-        return () => console.log('home destroyed')
-    }, [])
-
     return (
-        <Box className={"flex flex-col gap-4 items-center px-2  min-h-[100px] w-full" + (isMangaDetailsPage ? " h-[10%] max-h-[100px] justify-center md:flex-row md:justify-between " : " h-screen justify-start pt-[10%]")}>
-            <Box className="flex gap-2 items-center ">
+        <Box className={"flex flex-col gap-4 items-center px-2  min-h-[100px] w-full transition-all duration-500 ease-in-out" + (isMangaDetailsPage ? " h-[10%] max-h-[100px] justify-center md:flex-row md:justify-between " : " h-screen justify-start pt-[10%]")}>
+            <Box className="flex gap-2 items-center min-w-[280px] transition-all duration-500 ease-in-out">
                 <CustomImageViewer imgSrc={HeaderImage} boxProps={{ onClick: onClickHeader, cursor: 'pointer' }} />
                 <Text className="text-2xl font-bold cursor-pointer" onClick={onClickHeader}>
                     MangaHarbor
