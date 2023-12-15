78cc30838d0cd2fcf39167f290294eaf69ed3695
Sai Barath <saibarathdeep@outlook.com>
2023-12-15 13:01:53 +0530

    Replace CustomImageViewer with DisplayImage in Header.jsx

--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -1,6 +1,6 @@
 import { Box, Text } from "@chakra-ui/react";
 import SearchBar from "./commonComponents/SearchBar";
-import CustomImageViewer from "./commonComponents/ImageLoade";
+import DisplayImage from "./commonComponents/DisplayImage";
 import HeaderImage from '../icons/headerIcon.png'
 import { ThemeSwitcher } from "./commonComponents/ModeSwitcherButton";
 import { useLocation, useNavigate } from "react-router-dom";
@@ -15,7 +15,7 @@ const Header = () => {
     return (
         <Box className={"flex flex-col gap-4 items-center px-2  min-h-[100px] w-full transition-all duration-500 ease-in-out" + (isMangaDetailsPage ? " h-[10%] max-h-[100px] justify-center md:flex-row md:justify-between " : " h-screen justify-start pt-[10%]")}>
             <Box className="flex gap-2 items-center min-w-[280px] transition-all duration-500 ease-in-out">
-                <CustomImageViewer imgSrc={HeaderImage} boxProps={{ onClick: onClickHeader, cursor: 'pointer' }} />
+                <DisplayImage imgSrc={HeaderImage} boxProps={{ onClick: onClickHeader, cursor: 'pointer' }} />
                 <Text className="text-2xl font-bold cursor-pointer" onClick={onClickHeader}>
                     MangaHarbor
                 </Text>
