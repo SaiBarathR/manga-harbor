d11364ad8aff1e95171ace8aaa3a9680e84ddcd1
Sai Barath <saibarathr@gmail.com>
2023-12-27 12:14:44 +0530

    Refactor Loader component to use Chakra UI Spinner and Text

--- src/common/Loader.jsx
+++ src/common/Loader.jsx
@@ -1,7 +1,20 @@
+import { Spinner } from '@chakra-ui/react'
+import { Text } from '@chakra-ui/react'
+
 export default function Loader() {
     return (
-        <div className="flex justify-center items-center h-screen">
-            <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+        <div className="flex justify-center items-center h-screen gap-4 flex-col md:flex-row">
+            <Spinner
+                thickness='4px'
+                speed='0.65s'
+                emptyColor='gray.800'
+                color='blue.500'
+                size='xl'
+            />
+            <Text className="text-2xl font-bold text-center md:text-left">
+                Give us a moment, we are preparing your manga
+            </Text>    
         </div>
+
     )
 }
